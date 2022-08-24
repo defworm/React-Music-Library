bafc31529dfe34e68d416e2b74ad2827f8eab4ae
Crystal Korth <crystalkorth@gmail.com>
2022-08-24 18:30:52 -0500

    refactoring

--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -1,57 +1,68 @@
 import { useState } from 'react'
-
 function GalleryItem(props) {
     let [view, setView] = useState(false)
-
     const simpleStyle = {
-        'width': '25vw',
-        'height': '20vh',
-        'border': '1px solid black',
-        'margin': '2px'
-    }
-    
-    const detailStyle = {
         'width': '80vw',
-        'height': '20vh',
-        'border': '1px solid black',
-        'margin': '2px',
-        'backgroundImage': `url(${props.item.artworkUrl100})`,
-        'backgroundRepeat': 'no-repeat',
-        'backgroundSize': 'cover',
-        'color': 'yellow'
+        /* height: 20vh; */
+        border: '2px solid lightblue',
+        borderRadius: '9px',
+        margin: '.5%'
     }
     
+    const textStyles = {
+        detailText: {
+            flex: '25%',
+            margin: '0px',
+            color: 'black',
+            fontSize: '10px'
+        }
+    }
+    const detailStyle = {
+        outerDetail: {
+            'display': 'flex',
+            'width': '80vw',
+            'height': '40vh',
+            'border': '1px solid black',
+            'margin': '2px',
+            'backgroundImage': `url(${props.item.artworkUrl100})`,
+        },
+        innerDetail: {
+            width: '100%',
+            display: 'flex',
+            margin: '8% 1%',
+            backgroundColor: 'grey',
+            opacity: '0.9',
+            borderRadius: '10px',
+
+        }
+    }
 
     const simpleView = () => {
         return (
             <div style={simpleStyle}>
                 <h3>{props.item.trackName}</h3>
-                <h4>{props.item.collectionName}</h4>
+                {/* <h4>{props.item.collectionName}</h4> */}
+            </div>
+        )
+    }
+    const detailView = () => {
+        return (
+            <div style={detailStyle.outerDetail}>
+                <div style={detailStyle.innerDetail}>
+                    <h4 style={textStyles.detailText}>{props.item.trackName}</h4>
+                    <h4 style={textStyles.detailText}>{props.item.collectionName}</h4>
+                    <h4 style={textStyles.detailText}>{props.item.primaryGenreName}</h4>
+                    <h4 style={textStyles.detailText}>{props.item.releaseDate}</h4>
+                </div>
             </div>
         )
     }
     
-
-    const detailView = () => {
-        return (
-            <div>
-                <h2>{props.item.trackName}</h2>
-                <h3>{props.item.collectionName}</h3>
-                <h4>{props.item.primaryGenreName}</h4>
-                <h4>{props.item.releaseDate}</h4>
-            </div>
-        )
-    }
-
     return (
-        <div onClick={() => setView(!view)}
-        style={{'display': 'inline-block'}}>
-        
+        <div onClick={() => setView(!view)} style={{'display': 'inline-block'}}>
             {/* This simple ternary shows the simple view when 'view' is false! */}
             {view ? detailView() : simpleView()}
-
         </div>
     )
-
 }
 export default GalleryItem
