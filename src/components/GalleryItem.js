a652ec2d41a1fdcde8075e1af4daad96b919eee5
Crystal Korth <crystalkorth@gmail.com>
2022-08-25 22:34:40 -0500

    app does not work after doing activity rr-5. Tried class code, and redoing

--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -1,4 +1,7 @@
-import { useState } from 'react'
+// import { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+
 function GalleryItem(props) {
     let [view, setView] = useState(false)
     const simpleStyle = {
@@ -47,16 +50,24 @@ function GalleryItem(props) {
     }
     const detailView = () => {
         return (
-            <div style={detailStyle.outerDetail}>
-                <div style={detailStyle.innerDetail}>
-                    <h4 style={textStyles.detailText}>{props.item.trackName}</h4>
-                    <h4 style={textStyles.detailText}>{props.item.collectionName}</h4>
-                    <h4 style={textStyles.detailText}>{props.item.primaryGenreName}</h4>
-                    <h4 style={textStyles.detailText}>{props.item.releaseDate}</h4>
-                </div>
+            <div style={detailStyle}>
+                <h2>{props.item.trackName}</h2>
+                <h3>
+                    <Link to={`/artist/${props.item.artistId}`}>
+                        {props.item.artistName}
+                    </Link>
+                </h3>
+                <h3>
+                    <Link to={`/album/${props.item.collectionId}`}>
+                        {props.item.collectionName}
+                    </Link>
+                </h3>
+                <h4>{props.item.primaryGenreName}</h4>
+                <h4>{props.item.releaseDate}</h4>
             </div>
         )
     }
+    
     
     return (
         <div onClick={() => setView(!view)} style={{'display': 'inline-block'}}>
